59392dfa44bedbbb6d37cc84b7b517f5751908e0
Rishi Vora <ms21113@iisermohali.ac.in>
2022-09-28 22:21:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const imgs = document.getElementsByClassName(".wrapper");
+
+console.log(imgs.length);
+/*
+imgs.forEach(d => {
+    d.addEventListener('click', function () {
+        d.classList.toggle('test');
+    });
+})
+*/
